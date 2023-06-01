bd2618b6f2ce9d193affb35e873ca5a1d78358a8
Artur Michalski <armic@eg.dk>
2023-06-01 23:51:47 +0200

    Add appending test results to text file

--- HungarianAlgorithm/TestRunner/Program.cs
+++ HungarianAlgorithm/TestRunner/Program.cs
@@ -2,6 +2,7 @@
 using Hungarian;
 using Hungarian.Algorithms;
 using System.Diagnostics;
+using System.IO;
 
 namespace TestRunner
 {
@@ -11,31 +12,27 @@ namespace TestRunner
         {
             int[] seeds = { 420, 9999, 12345 };
             int numberOfRuns = 3;
+            string testResultsFilePath = "../../../../Application/Output/TestResults.txt";
 
             (int N, IEnumerable<int> Ks)[] tests = new (int N, IEnumerable<int> Ks)[]
             {
-                (500, new int[]{2}),
+                (1, Enumerable.Range(1, 10)),
+                (2, Enumerable.Range(1, 10)),
+                (3, Enumerable.Range(1, 10)),
+                (4, Enumerable.Range(1, 10)),
+                (5, Enumerable.Range(1, 10)),
+                (6, Enumerable.Range(1, 10)),
+                (7, Enumerable.Range(1, 10)),
+                (8, Enumerable.Range(1, 10)),
+                (9, Enumerable.Range(1, 10)),
+                (10, Enumerable.Range(1, 10)),
+                (20, Enumerable.Range(1, 10)),
+                (50, Enumerable.Range(1, 5)),
+                (100, Enumerable.Range(1, 5)),
+                (200, Enumerable.Range(1, 5)),
+                (500, Enumerable.Range(1, 3)),
+                (1000, Enumerable.Range(1, 2)),
             };
-
-            //(int N, IEnumerable<int> Ks)[] tests = new (int N, IEnumerable<int> Ks)[]
-            //{
-            //    (1, Enumerable.Range(1, 10)),
-            //    (2, Enumerable.Range(1, 10)),
-            //    (3, Enumerable.Range(1, 10)),
-            //    (4, Enumerable.Range(1, 10)),
-            //    (5, Enumerable.Range(1, 10)),
-            //    (6, Enumerable.Range(1, 10)),
-            //    (7, Enumerable.Range(1, 10)),
-            //    (8, Enumerable.Range(1, 10)),
-            //    (9, Enumerable.Range(1, 10)),
-            //    (10, Enumerable.Range(1, 10)),
-            //    (20, Enumerable.Range(1, 10)),
-            //    (50, Enumerable.Range(1, 4)),
-            //    (100, Enumerable.Range(1, 4)),
-            //    (200, Enumerable.Range(1, 4)),
-            //    (500, Enumerable.Range(1, 4)),
-            //    (1000, Enumerable.Range(1, 2)),
-            //};
             int totalTestsCount = tests.Sum(test => test.Ks.Count());
 
             int testIndex = 1;
@@ -48,16 +45,19 @@ namespace TestRunner
                     while (run++ < numberOfRuns)
                     {
                         Console.WriteLine($"Run {run}:");
-                        ExecuteTestAndGetAlgorithmsExecutionTimes(test.N, K, run, seeds[run-1]);
+                        (TimeSpan hungarianExecutionTime, TimeSpan libraryExecutionTime, bool doesAssignmentCostMatch) = ExecuteTestAndGetAlgorithmsExecutionTimes(test.N, K, run, seeds[run-1]);
+                        using (StreamWriter sw = File.AppendText(testResultsFilePath))
+                        {
+                            sw.WriteLine($"{test.N}, {K}, {hungarianExecutionTime}, {libraryExecutionTime}, {doesAssignmentCostMatch}");
+                        }
+
                     }
                     testIndex++;
                 }
             }
-
-            //Computation time (N=100, K=4): 1 minute for three runs
         }
 
-        private static (TimeSpan hungarianExecutionTime, TimeSpan libraryExecutionTime, bool isAssignmentCostDifferent) ExecuteTestAndGetAlgorithmsExecutionTimes(int N, int K, int run, int seed)
+        private static (TimeSpan hungarianExecutionTime, TimeSpan libraryExecutionTime, bool doesAssignmentCostMatch) ExecuteTestAndGetAlgorithmsExecutionTimes(int N, int K, int run, int seed)
         {
             string inputFileName = $"../../../../Application/Input/{N}_{K}_{run}.txt";
             string outputFileName = inputFileName.Replace("Input", "Output");
@@ -73,22 +73,22 @@ namespace TestRunner
 
             var timer = new Stopwatch();
             timer.Start();
-            Solution hungarian_solution = hungarian.Solve(distances);
+            Solution hungarianSolution = hungarian.Solve(distances);
             timer.Stop();
             TimeSpan hungarianExecutionTime = timer.Elapsed;
             Console.WriteLine("Hungarian: " + hungarianExecutionTime.ToString(@"m\:ss\.fff"));
 
             timer = new Stopwatch();
             timer.Start();
-            Solution library_solution = library.Solve(distances);
+            Solution librarySolution = library.Solve(distances);
             timer.Stop();
             TimeSpan libraryExecutionTime = timer.Elapsed;
             Console.WriteLine("Library: " + libraryExecutionTime.ToString(@"m\:ss\.fff"));
 
-            FileWriter.WriteToOutputFile(outputFileName.Replace(".txt", "_h.txt"), hungarian_solution);
-            FileWriter.WriteToOutputFile(outputFileName.Replace(".txt", "_l.txt"), library_solution);
+            FileWriter.WriteToOutputFile(outputFileName.Replace(".txt", "_h.txt"), hungarianSolution);
+            FileWriter.WriteToOutputFile(outputFileName.Replace(".txt", "_l.txt"), librarySolution);
 
-            return (hungarianExecutionTime, libraryExecutionTime, 3 != library_solution.TotalAssignmentCost);
+            return (hungarianExecutionTime, libraryExecutionTime, hungarianSolution.TotalAssignmentCost == librarySolution.TotalAssignmentCost);
         }
     }
 }
